ea6173aaa32820e3be4bfade1ad47bc34650d36a
wclwilliam <williamlee912@gmail.com>
2025-12-04 10:10:23 +0800

    20251204 ver3 (index rwd-576-half)

---
+++ js/general.js
@@ -0,0 +1,21 @@
+// 漢堡
+// 1. 點選漢堡時toggle "active"
+// 2. 點選漢堡內任一連結時，remove "active"
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const hamburger = document.querySelector("header .hamburger");
+    const nav = document.querySelector("header nav");
+
+    hamburger.addEventListener("click", function() {
+        hamburger.classList.toggle("active");
+        nav.classList.toggle("active");
+    });
+
+    document.querySelectorAll("header nav li a").forEach(link => {
+        link.addEventListener("click", () => {
+            hamburger.classList.remove("active");
+            nav.classList.remove("active");
+        });
+    });
+});
